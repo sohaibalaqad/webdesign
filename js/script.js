398258561192595621e4d3caeb66656f455382df
sohaib alaqad <45884646+sohaibalaqad@users.noreply.github.com>
2020-02-11 01:31:59 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// var id = ["mohammed", 15, "male" , true];
+// for(var count = 0 ; count<id.length ; count++){
+//     document.getElementById("math").innerHTML += id[count] + " <br>"
+// }
+
+// for (var count = 0; count <= 10; ++count) {
+//     document.getElementById("math").innerHTML += count + " <br>"
+// }
+
+// var x = 1 ;
+// while(x <=10) {
+//     if (x == 6){
+//         continue;
+//     }
+//     document.getElementById("math").innerHTML += x;
+//     ++x;
+// }
+
+
+// for ( var i = 0 ; i <=10 ; ++i){
+//     if (i == 5 ){
+//         continue;
+//     }
+//     document.getElementById("math").innerHTML += i ;
+// }
+
+
+// var count = 1 ;
+
+// do {
+//     document.getElementById("math").innerHTML += count + " <br>" ;
+//     count++;
+// } while (count >= 5);
+
+
+function play(name, age) {
+    console.log(name , age);
+}
